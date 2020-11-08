1df690cf312c4af10ad91ca42c58bf39b72a7b17
Lukas Siarny <lukas.siarny@gmail.com>
2020-11-08 18:50:47 +0100

    DB host changed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 require("./config/passport")(passport); 
 
 //connect to DB
-mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Mongo DB connected"))
 .catch((err) => console.log(err));
 
